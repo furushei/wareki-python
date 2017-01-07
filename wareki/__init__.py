5619fc765b94c1668929b6af93de7f14cc75435a
古川貴之 <t.furukawa.1025@gmail.com>
2017-01-07 12:12:12 +0900

    ソースコードに説明を追加

--- wareki/__init__.py
+++ wareki/__init__.py
@@ -6,12 +6,35 @@
 Author: 古川貴之
 
 
+和暦の表現方法
+
+>>> Wareki(Gengo('平成'), 32)
+Wareki(Gengo('平成'), 32)
+
+>>> S(39)
+Wareki(Gengo('昭和'), 39)
+
+>>> str(H(32))
+'平成32年'
+
+
+西暦を和暦に変換する
+
+>>> Wareki.from_ad(2020)
+Wareki(Gengo('平成'), 32)
+
+
+和暦を西暦に変換する
+
+>>> H(32).to_ad()
+2020
+
+# 次のように、int()に渡すこともできます。
 >>> int(H(32))
 2020
 """
 
 import datetime as _datetime
-
 
 _gengo_table = (
     {
